39c9e89418504384b1e779908af0586fa2c5d8f2
i6294250-design <i6294250@gmail.com>
2026-02-03 11:12:45 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// 책 데이터
+const books = {
+  "1": {
+    title: "예제 책 1",
+    pages: [
+      "images/1/1.jpg",
+      "images/1/2.jpg",
+      "images/1/3.jpg"
+    ]
+  },
+  "2": {
+    title: "예제 책 2",
+    pages: [
+      "images/2/1.jpg",
+      "images/2/2.jpg"
+    ]
+  }
+};
